9a517e5c384a19f48a09064b9abbf59fc5484158
Regis Lima <regislima1391@gmail.com>
2019-02-07 14:20:47 -0300

    Definida a classe Worker com suas devidas propriedades associadas e seus construtores

---
+++ WorkerPayment/Entities/Worker.cs
@@ -0,0 +1,27 @@
+﻿using WorkerPayment.Entities.Enums;
+using System.Collections.Generic;
+
+namespace WorkerPayment.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+    }
+}
